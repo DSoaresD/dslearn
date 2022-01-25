31a7db1bc709d2667145f3e133b524163a751546
Daniel Soares <danielsoares321@gmail.com>
2022-01-25 01:24:33 -0300

    Enrollment seed

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -26,8 +26,8 @@ INSERT INTO tb_section (title, description, position,img_Uri, resource_id, prere
 INSERT INTO tb_section (title, description, position,img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2','Neste capitulo vamos contiuar', 2, 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 1, 1);
 INSERT INTO tb_section (title, description, position,img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3','Neste capitulo vamos finalizar', 3, 'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d', 1, 2);
 
-
-
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1,TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1,TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
 
 
 
